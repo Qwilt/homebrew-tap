b035b0cd130445213e3bf7492bc91fe2b5b8a662
Rafael Bodill <justrafi@gmail.com>
2025-07-07 15:49:21 +0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Qwilt Homebrew Tap
+
+Homebrew formulae published by Qwilt. Usage:
+
+```sh
+brew tap qwilt/tap
+```
+
+## Included Software
+
+- [qctl](https://github.com/Qwilt/qctl) - qclt CLI:
+
+  ```sh
+  brew install qwilt/tap/qctl
+  ```
---
+++ qctl.rb
@@ -0,0 +1,25 @@
+class Qctl < Formula
+  desc "Qwilt Open Edge CLI"
+  homepage "https://github.com/Qwilt/qctl"
+  url "https://github.com/Qwilt/qctl/archive/refs/tags/v0.11.2.tar.gz"
+  version "0.11.2"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/Qwilt/qctl/releases/download/v#{version}/qctl_darwin_arm64"
+    sha256 "f3c13a8c3688345fb8867da654df8d81f070a15b40bae947a6578fbe835ee6b5"
+  else
+    url "https://github.com/Qwilt/qctl/releases/download/v#{version}/qctl_darwin_amd64"
+    sha256 "7d4d9334576116ea9d13322142f2e2905739641b29ae0da6a7615c7c6bdb22d7"
+  end
+
+  def install
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    os = OS.kernel_name.downcase
+
+    bin.install "qctl_#{os}_#{arch}" => "qctl"
+  end
+
+  test do
+    system "#{bin}/qctl", "version"
+  end
+end
