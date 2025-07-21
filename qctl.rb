51c69d3d2a58b7a3c3ec1eba73a7e3899597e5fb
qwiltBot <qwiltBot@qwilt.com>
2025-07-21 08:02:38 +0000

    chore: track qctl version

--- qctl.rb
+++ qctl.rb
@@ -2,14 +2,14 @@ class Qctl < Formula
   desc "Qwilt Open Edge CLI"
   homepage "https://github.com/Qwilt/qctl"
   url "https://github.com/Qwilt/qctl/archive/refs/tags/v#{version}.tar.gz"
-  version "0.18.0"
+  version "0.19.0"
 
   if Hardware::CPU.arm?
     url "https://github.com/Qwilt/qctl/releases/download/v#{version}/qctl_darwin_arm64"
-    sha256 "69ec312c1e09ca3d62adcd450bbe52a9c343958645457d1b037b197bdfbf3576"
+    sha256 "586b1958851826a677a55704846e0bd1d88f7dec873686968c4a62538efaecc3"
   else
     url "https://github.com/Qwilt/qctl/releases/download/v#{version}/qctl_darwin_amd64"
-    sha256 "13b02512c3b6289f9bc74a9a00c0f3b21cf1275345def51693ac341c03589137"
+    sha256 "bbc3d78e360a309b4547a32866e90f5a8feed6c73ec3124325fe70cb79a84e2f"
   end
 
   def install
